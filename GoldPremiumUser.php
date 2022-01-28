fb6b9339b25c983a287d4e0ccfadb766a8c9e370
Albaer33 <alisi3396@gmail.com>
2022-01-28 16:56:29 +0100

    users added all product to their basket

--- GoldPremiumUser.php
+++ GoldPremiumUser.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/User.php';
 class GoldPremiumUser extends User {
     private $shipping_price = 0;
     private $discount = 20;
-
+    
     public function getShippingPrice() {
         return $this->shipping_price;
     }
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/User.php';
 require_once __DIR__ . '/Product.php';
+require_once __DIR__ . '/StandardUser.php';
+require_once __DIR__ . '/PremiumUser.php';
+require_once __DIR__ . '/GoldPremiumUser.php';
 
 // creo classi prodotto, genero e setto i relativi codici
 $prod1 = new Product('Samsung S21', 850);
@@ -12,6 +15,21 @@ $prod2->generateSetCode();
 $prod3 = new Product('Asus PC da gaming', 1600);
 $prod3->generateSetCode();
 
-// creo 2 classi utenti, 1 standard, 1 premium, 1 gold premium
+// creo 3 classi utenti, 1 standard, 1 premium, 1 gold premium
+$user1 = new StandardUser('alberto', 'lisi', 'alisi3396@gmail.com');
+$user2 = new PremiumUser('marco', 'focamento', 'focamento@gmail.com');
+$user3 = new GoldPremiumUser('pasquale', 'direraven', 'nonmelaricordo@gmail.com');
 
+// aggiungo tutti i prodotti a tutti gli user
+$user1->addProduct($prod1);
+$user1->addProduct($prod2);
+$user1->addProduct($prod3);
+
+$user2->addProduct($prod1);
+$user2->addProduct($prod2);
+$user2->addProduct($prod3);
+
+$user3->addProduct($prod1);
+$user3->addProduct($prod2);
+$user3->addProduct($prod3);
 ?>
